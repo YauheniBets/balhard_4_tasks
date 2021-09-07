4a612c2bc507c98bc805caa3f068fec319dcc6c0
Evgeniy Bets <n3shta2007@yandex.ru>
2021-09-07 14:46:17 +0300

    collection is list type now

--- tasks/list_tasks/copy_list.py
+++ tasks/list_tasks/copy_list.py
@@ -23,8 +23,8 @@ user_data_template = [
 
 
 def copy_list(collection: list) -> list:
-    collection = list(user_data_template.copy())
-    collection_copy = (collection)
+
+    collection_copy = list(collection)
     return collection_copy
 
 
